053c116e0caa9192c5f20273cec7ce37ff7f9992
ddrsmiLe <ddrsmile@gmail.com>
2017-10-12 01:52:35 +0900

    refactored c_cpp runner

--- src/coderunner/workplace/c_cpp/parser/src/Factory.cpp
+++ src/coderunner/workplace/c_cpp/parser/src/Factory.cpp
@@ -17,6 +17,11 @@ IParser<T>* Factory::create(const string& name) {
     throw "Factory::create: key was not found in hash map.  Did you forget to register it?";
 }
 
+// the types to be used should be declared explicitly for templates.
+template class IParser<int>* Factory::create(const string& name);
+template class IParser<double>* Factory::create(const string& name);
+template class IParser<string>* Factory::create(const string& name);
+
 void Factory::Register(const string& name, CreateObjFn pCreateFn) {
     registered_map[name] = pCreateFn;
 }
