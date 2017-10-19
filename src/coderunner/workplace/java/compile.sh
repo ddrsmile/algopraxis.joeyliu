0ad11860827afb2e31055b52c79b4cfa1b944cb5
ddrsmiLe <ddrsmile@gmail.com>
2017-10-20 01:35:54 +0900

    updated bash scripts

--- src/coderunner/workplace/java/compile.sh
+++ src/coderunner/workplace/java/compile.sh
@@ -9,9 +9,8 @@ fi
 BIN_DIR=$ROOT_DIR/bin
 CLASSPATH=.:$ROOT_DIR/bin
 
-# src and input files' directories
+# src directories
 SRC_DIR=$ROOT_DIR/src
-INPUT_DIR=$ROOT_DIR/input
 
 arg=$1
 
@@ -19,58 +18,27 @@ if [ "$arg" = 0 ]; then
   exit 0
 fi
 
-if [ -z "$ROOT_DIR" -o ! "$ROOT_DIR"=`pwd`  ]; then
-  echo "Please resouce set_env.sh"
+
+if [ "$arg" = "aux" ]; then
+  echo "compile auxiliary..."
+  javac -d $BIN_DIR -cp $CLASSPATH $(find $SRC_DIR/auxiliary -name '*.java')
+  echo "completed..."
   exit 0
 fi
 
-#collect the item not compiled
-function check {
-  DIR=$1
-  cnt=0
-  for fname in $(find $SRC_DIR/$DIR -name '*.java')
-  do
-    fname="${fname/$SRC_DIR/$BIN_DIR}"
-    fname="${fname/.java/.class}"
-    if [ ! -f $fname ]; then
-      cnt=$(($cnt+1))
-    fi
-  done
-  echo $cnt
-}
-
-
-if [ "$arg" = "objs" ]; then
-  javac -d $BIN_DIR -cp $CLASSPATH $(find $SRC_DIR/objs -name '*.java')
+if [ "$arg" = "parser" ]; then
+  echo "compile parser..."
+  javac -d $BIN_DIR -cp $CLASSPATH $(find $SRC_DIR/parser -name '*.java')
+  echo "completed..."
   exit 0
 fi
-
-if [ "$arg" = "utils" ]; then
-  ckobjs=$(check "objs")
-  if [ ! $ckobjs = 0 ]; then
-    javac -d $BIN_DIR -cp $CLASSPATH $(find $SRC_DIR/objs -name '*.java')
-  fi
-  javac -d $BIN_DIR -cp $CLASSPATH $(find $SRC_DIR/utils -name '*.java')
-  exit 0
-fi
-
 
 if [ "$arg" = "all" ] || [ -z $arg ]; then
-  echo "compile both objs and utils..."
-  ckobjs=$(check "objs")
-  if [ ! $ckobjs = 0 ]; then
-    echo "compile objs..."
-    javac -d $BIN_DIR -cp $CLASSPATH $(find $SRC_DIR/objs -name '*.java')
-  else
-    echo "objs have been compiled..."
-  fi
-  ckutils=$(check "utils")
-  if [ ! $ckutils = 0 ]; then
-    echo "compile utils..."
-    javac -d $BIN_DIR -cp $CLASSPATH $(find $SRC_DIR/utils -name '*.java')
-  else
-    echo "utils have been compiled..."
-  fi
+  echo "compile both auxiliary and parser..."
+  echo "compile auxiliary..."
+  javac -d $BIN_DIR -cp $CLASSPATH $(find $SRC_DIR/auxiliary -name '*.java')
+  echo "compile parser..."
+  javac -d $BIN_DIR -cp $CLASSPATH $(find $SRC_DIR/parser -name '*.java')
   echo "completed..."
   exit 0
 fi
