481cc3947cc50484d08b4780f03a7abe3cff43c6
Joey Liu <nantsou.liu@gmail.com>
2020-06-08 23:35:28 +0900

    clear unused import module in auth/views

--- api/jwt_auth/views.py
+++ api/jwt_auth/views.py
@@ -1,16 +1,7 @@
 # -*- coding: utf-8 -*-
 from rest_framework import status
-from rest_framework.decorators import (
-    api_view,
-    authentication_classes,
-    permission_classes,
-)
-from rest_framework.permissions import IsAuthenticated
+from rest_framework.decorators import api_view
 from rest_framework.response import Response
-
-from ..jwt_auth.authentication import (
-    JWTAuthentication,
-)
 
 from .serializers import (
     TokenAcquireSerializer,
