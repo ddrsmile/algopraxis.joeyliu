a95a03e20244546ae6e7e1d4c70101ab07684eee
Joey Liu <nantsou.liu@gmail.com>
2020-06-09 00:00:21 +0900

    rename auth module

--- api/urls.py
+++ api/urls.py
@@ -6,7 +6,7 @@ from django.urls import (
 
 urlpatterns = [
     # auth
-    re_path(r'^auth/', include(('api.jwt_auth.urls', 'api.auth'), namespace='auth')),
+    re_path(r'^auth/', include(('api.auth.urls', 'api.auth'), namespace='auth')),
     # api_v1
     re_path(r'^v1/', include(('api.v1.urls', 'api.v1'), namespace='v1')),
 ]
